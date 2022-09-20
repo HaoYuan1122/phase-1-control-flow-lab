e4ecb37cb5e42d217554efe65c5fe85cd3289ad3
Hao Yuan <haomichaelyuan@gmail.com>
2022-09-20 16:38:15 +1000

    commit tree 8f4f4130f9cb956690f7cf8b49ba6fd2f7f987a5

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
+  if (a<400){
+    return 'This one is on me!'
+  } else if(a>400 && a<=2000){
+    return 'That will be twenty bucks.'
+  } else if (a>2000 && a<=2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(a){
   // Write your code here!
+    return a==="NYC"?'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(a){
   // Write your code here!
+    if (a==='generous'){
+        return 'Thank you so much.'
+    } else if(a!=='generous'){
+        return 'Thank you.'
+    } else{
+        return 'Bye.'
+    }
 }
